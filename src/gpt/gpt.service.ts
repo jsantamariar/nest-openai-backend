445d65a22eba6cc74d50e2f42503e81889e2e177
“jsantamariar” <santamaria.web.dev@gmail.com>
2024-12-01 10:57:25 -0500

    first commit

---
+++ src/gpt/gpt.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { orthographyCheckUseCase } from './use-cases';
+import { OrthographyDto } from './dtos';
+
+@Injectable()
+export class GptService {
+  async orthographyCheck(orthographyDto: OrthographyDto) {
+    return await orthographyCheckUseCase({ prompt: orthographyDto.prompt });
+  }
+}
